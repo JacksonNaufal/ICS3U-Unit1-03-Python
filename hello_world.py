23fea3b7e5165c8c56c371fbd6f7637b04331e4a
Jackson Naufal <Jackson.naufal@mths.ca>
2022-02-17 19:45:59 +0000

    HelloWorldProgram

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: Feb 2022
+# This is a "Hello, World! program, with proper style"
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
